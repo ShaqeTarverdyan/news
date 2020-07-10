4dcb5970ad63667a82b2b8c793d9dcb3de4d7f3e
shaqe <tshaqe@gmail.com>
2020-07-10 14:00:17 +0400

    Updates

---
+++ precache-manifest.9a73d769b16610a8e65ce4726593e4b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce8e20c79190da64ec650322b3c29637",
+    "url": "/ShaqeTarverdyan/news-app/index.html"
+  },
+  {
+    "revision": "be47a63116c0eb69c351",
+    "url": "/ShaqeTarverdyan/news-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9c98af5d2e869552cb68",
+    "url": "/ShaqeTarverdyan/news-app/static/js/2.91183502.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ShaqeTarverdyan/news-app/static/js/2.91183502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be47a63116c0eb69c351",
+    "url": "/ShaqeTarverdyan/news-app/static/js/main.8165eb96.chunk.js"
+  },
+  {
+    "revision": "0a55122082c9b716625e",
+    "url": "/ShaqeTarverdyan/news-app/static/js/runtime-main.a2fec681.js"
+  }
+]);
